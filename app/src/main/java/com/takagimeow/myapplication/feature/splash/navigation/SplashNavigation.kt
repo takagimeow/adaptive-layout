c6af605b930743f97ad55c8f0fc71594a26ade3e
Keisuke Takagi <mit@takagi.app>
2023-02-03 10:16:15 +0900

    chore: SplashNavigation created.

---
+++ app/src/main/java/com/takagimeow/myapplication/feature/splash/navigation/SplashNavigation.kt
@@ -0,0 +1,31 @@
+package com.takagimeow.myapplication.feature.splash.navigation
+
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.takagimeow.adaptivelayout.AdaptiveLayoutNavigationDestination
+import com.takagimeow.myapplication.feature.splash.SplashRoute
+
+object SplashDestination : AdaptiveLayoutNavigationDestination {
+    override val route = "splash_route"
+    override val destination = "splash_destination"
+}
+
+fun NavGraphBuilder.splashGraph(
+    navigateToHome: (NavBackStackEntry) -> Unit,
+    navigateToOnboarding: (NavBackStackEntry) -> Unit,
+) {
+    composable(
+        route = SplashDestination.route
+    ) {
+            navBackStackEntry ->
+        SplashRoute(
+            navigateToHome = {
+                navigateToHome(navBackStackEntry)
+            },
+            navigateToOnboarding = {
+                navigateToOnboarding(navBackStackEntry)
+            }
+        )
+    }
+}
