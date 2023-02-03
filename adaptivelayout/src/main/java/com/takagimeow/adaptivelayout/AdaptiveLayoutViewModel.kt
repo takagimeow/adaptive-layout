3df009afb213859abdeb0e9efe76fdf05e509e94
Keisuke Takagi <mit@takagi.app>
2023-02-03 09:25:49 +0900

    feat: AdaptiveLayoutViewModel created.

---
+++ adaptivelayout/src/main/java/com/takagimeow/adaptivelayout/AdaptiveLayoutViewModel.kt
@@ -0,0 +1,53 @@
+package com.takagimeow.adaptivelayout
+
+import androidx.activity.ComponentActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.window.layout.FoldingFeature
+import androidx.window.layout.WindowInfoTracker
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+data class AdaptiveLayoutUiState(
+    val isAuthenticated: Boolean = false,
+)
+
+class AdaptiveLayoutViewModel (
+    val activity: ComponentActivity
+): ViewModel() {
+    private val _uiState =
+        MutableStateFlow(
+            AdaptiveLayoutUiState(
+                isAuthenticated = false
+            )
+        )
+    val uiState: StateFlow<AdaptiveLayoutUiState> = _uiState.asStateFlow()
+
+    // Check and monitor folding state.
+    val devicePostureFlow = WindowInfoTracker.getOrCreate(activity)
+        .windowLayoutInfo(activity)
+        .flowWithLifecycle(activity.lifecycle) // Supported since lifecycle-viewmodel-compose:2.5.1
+        .map { layoutInfo ->
+            val foldingFeature =
+                layoutInfo.displayFeatures
+                    .filterIsInstance<FoldingFeature>()
+                    .firstOrNull()
+            when {
+                isBookPosture(foldingFeature) ->
+                    DevicePosture.BookPosture(foldingFeature.bounds)
+                isSeparating(foldingFeature) ->
+                    DevicePosture.Separating(foldingFeature.bounds, foldingFeature.orientation)
+                else -> DevicePosture.NormalPosture
+            }
+        }
+        .stateIn(
+            scope = activity.lifecycleScope,
+            started = SharingStarted.Eagerly,
+            initialValue = DevicePosture.NormalPosture
+        )
+}
