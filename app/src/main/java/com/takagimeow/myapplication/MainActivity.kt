463711e029d1041d00acf1591802bfec8a6ed160
Keisuke Takagi <mit@takagi.app>
2023-02-03 10:19:17 +0900

    chore: An example of AdaptiveLayout Composable created.

--- app/src/main/java/com/takagimeow/myapplication/MainActivity.kt
+++ app/src/main/java/com/takagimeow/myapplication/MainActivity.kt
@@ -1,46 +1,63 @@
 package com.takagimeow.myapplication
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.annotation.RequiresApi
+import com.takagimeow.adaptivelayout.AdaptiveLayout
+import com.takagimeow.adaptivelayout.AdaptiveLayoutViewModel
+import com.takagimeow.adaptivelayout.rememberAdaptiveLayoutAppState
+import com.takagimeow.myapplication.feature.splash.navigation.SplashDestination
+import com.takagimeow.myapplication.ui.HomeTopLevelDestination
+import com.takagimeow.myapplication.ui.MyApplicationNavGraph
+import com.takagimeow.myapplication.ui.SettingsTopLevelDestination
 import com.takagimeow.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+            val appState = rememberAdaptiveLayoutAppState(
+                topLevelDestinations = listOf(
+                    HomeTopLevelDestination,
+                    SettingsTopLevelDestination,
+                ),
+            )
+            val viewModel = AdaptiveLayoutViewModel(activity = this)
+
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                AdaptiveLayout(
+                    appState = appState,
+                    viewModel = viewModel,
+                    activity = this,
+                    background = { route, content ->
+                        when (route) {
+                            HomeTopLevelDestination.destination -> {
+                                content()
+                            }
+
+                            SettingsTopLevelDestination.destination -> {
+                                content()
+                            }
+
+                            else -> {
+                                content()
+                            }
+                        }
+                    },
+                ) { isListAndDetail ->
+                    MyApplicationNavGraph(
+                        navController = appState.navController,
+                        isListAndDetail = isListAndDetail,
+                        startDestination = SplashDestination.route,
+                        onNavigateAndPopUpToDestination = appState::navigateAndPopUp,
+                        onNavigateToDestination = appState::navigate,
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }
