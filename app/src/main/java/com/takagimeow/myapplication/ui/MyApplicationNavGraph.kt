990364179b77cce746a90588c4717aa3c64270f7
Keisuke Takagi <mit@takagi.app>
2023-02-04 18:06:36 +0900

    feat: MyApplicationNavGraph created.

---
+++ app/src/main/java/com/takagimeow/myapplication/ui/MyApplicationNavGraph.kt
@@ -0,0 +1,47 @@
+package com.takagimeow.myapplication.ui
+
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.takagimeow.adaptivelayout.AdaptiveLayoutNavigationDestination
+import com.takagimeow.myapplication.feature.home.navigation.homeGraph
+import com.takagimeow.myapplication.feature.settings.navigation.settingsGraph
+import com.takagimeow.myapplication.feature.splash.navigation.splashGraph
+
+private const val TAG = "MyApplicationNavGraph"
+@Composable
+fun MyApplicationNavGraph(
+    navController: NavHostController,
+    isListAndDetail: Boolean = false,
+    onNavigateToDestination: (AdaptiveLayoutNavigationDestination, String, from: NavBackStackEntry?) -> Unit,
+    onNavigateAndPopUpToDestination: (AdaptiveLayoutNavigationDestination, String, from: NavBackStackEntry?) -> Unit,
+    startDestination: String,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+    ) {
+        splashGraph(
+            navigateToHome = { navBackStackEntry ->
+                Log.d(TAG, "navigateToHomeが呼び出されました")
+                onNavigateAndPopUpToDestination(
+                    HomeTopLevelDestination,
+                    HomeTopLevelDestination.destination,
+                    navBackStackEntry
+                )
+            },
+            navigateToOnboarding = {
+                Log.d(TAG, "navigateToOnboardingが呼び出されました")
+            }
+        )
+        homeGraph(
+            isListAndDetail = isListAndDetail,
+        ) {}
+        settingsGraph(
+            isListAndDetail = isListAndDetail,
+        )
+    }
+}
