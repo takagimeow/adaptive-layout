bb341b15208a26098cb000dfb64554dfe0739bd3
Keisuke Takagi <mit@takagi.app>
2023-02-05 13:12:11 +0900

    refactor: Modified to set contentDescription.

--- adaptivelayout/src/main/java/com/takagimeow/adaptivelayout/AdaptiveLayoutNavigationRail.kt
+++ adaptivelayout/src/main/java/com/takagimeow/adaptivelayout/AdaptiveLayoutNavigationRail.kt
@@ -9,6 +9,9 @@ import androidx.compose.material3.NavigationRailItem
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 
 @Composable
 fun AdaptiveLayoutNavigationRail(
@@ -17,8 +20,14 @@ fun AdaptiveLayoutNavigationRail(
     onDrawerClicked: () -> Unit = {},
     onNavigate: (AdaptiveLayoutNavigationDestination) -> Unit,
 ) {
+    val navigationRailContentDescription = stringResource(id = R.string.navigation_rail)
+    val navigationRailDrawerIconContentDescription = stringResource(id = R.string.navigation_rail_drawer_icon)
     NavigationRail(
-        modifier = Modifier.fillMaxHeight()
+        modifier = Modifier
+            .fillMaxHeight()
+            .semantics {
+                contentDescription = navigationRailContentDescription
+            },
     ) {
         NavigationRailItem(
             selected = false,
@@ -26,7 +35,7 @@ fun AdaptiveLayoutNavigationRail(
             icon = {
                 Icon(
                     imageVector = Icons.Default.Menu,
-                    contentDescription = ""
+                    contentDescription = navigationRailDrawerIconContentDescription
                 )
             }
         )
@@ -39,14 +48,19 @@ fun AdaptiveLayoutNavigationRail(
                     } else {
                         destination.unselectedAdaptiveLayoutIcon
                     }
+                    val contentDescription = if (selected) {
+                        stringResource(id = R.string.navigation_rail_selected_icon, destination.route)
+                    } else {
+                        stringResource(id = R.string.navigation_rail_unselected_icon, destination.route)
+                    }
                     when (icon) {
                         is AdaptiveLayoutIcon.ImageVectorAdaptiveLayoutIcon -> Icon(
                             imageVector = icon.imageVector,
-                            contentDescription = destination.route
+                            contentDescription = contentDescription
                         )
                         is AdaptiveLayoutIcon.DrawableResourceAdaptiveLayoutIcon -> Icon(
                             painter = painterResource(id = icon.id),
-                            contentDescription = destination.route
+                            contentDescription = contentDescription
                         )
                     }
                 },
