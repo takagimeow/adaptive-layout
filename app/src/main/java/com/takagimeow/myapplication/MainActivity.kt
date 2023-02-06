72806cbacba9495555ea576fcb9658005da0e383
Keisuke Takagi <mit@takagi.app>
2023-02-06 19:53:21 +0900

    chore: the appState variable preparation has been removed.

--- app/src/main/java/com/takagimeow/myapplication/MainActivity.kt
+++ app/src/main/java/com/takagimeow/myapplication/MainActivity.kt
@@ -6,8 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import com.takagimeow.adaptivelayout.AdaptiveLayout
-import com.takagimeow.adaptivelayout.AdaptiveLayoutViewModel
-import com.takagimeow.adaptivelayout.rememberAdaptiveLayoutAppState
 import com.takagimeow.myapplication.feature.splash.navigation.SplashDestination
 import com.takagimeow.myapplication.ui.HomeTopLevelDestination
 import com.takagimeow.myapplication.ui.MyApplicationNavGraph
@@ -20,19 +18,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val appState = rememberAdaptiveLayoutAppState(
-                topLevelDestinations = listOf(
-                    HomeTopLevelDestination,
-                    SettingsTopLevelDestination,
-                ),
-            )
-            val viewModel = AdaptiveLayoutViewModel(activity = this)
-
             MyApplicationTheme {
                 AdaptiveLayout(
-                    appState = appState,
-                    viewModel = viewModel,
-                    activity = this,
+                    topLevelDestinations = listOf(
+                        HomeTopLevelDestination,
+                        SettingsTopLevelDestination,
+                    ),
                     background = { route, content ->
                         when (route) {
                             HomeTopLevelDestination.destination -> {
@@ -48,13 +39,13 @@ class MainActivity : ComponentActivity() {
                             }
                         }
                     },
-                ) { isListAndDetail ->
+                ) { isListAndDetail, navController, navigate, navigateAndPopUp ->
                     MyApplicationNavGraph(
-                        navController = appState.navController,
+                        navController = navController,
                         isListAndDetail = isListAndDetail,
                         startDestination = SplashDestination.route,
-                        onNavigateAndPopUpToDestination = appState::navigateAndPopUp,
-                        onNavigateToDestination = appState::navigate,
+                        onNavigateAndPopUpToDestination = navigateAndPopUp,
+                        onNavigateToDestination = navigate,
                     )
                 }
             }
